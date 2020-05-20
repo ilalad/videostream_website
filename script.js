f05e24403bd6b74cfe8f11cd5fdce83d1a6e34d3
Derrick Chargois <36555200+dchargois23@users.noreply.github.com>
2020-05-20 17:55:45 -0500

    update1

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("roger that!!!");
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=us",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+        "x-rapidapi-key": "230f5fd612msh4e36283b5d68e1bp179416jsnd53a23333929"
+    }
+}
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+});
+
+
+
+
+
+
+
+
