bf86104e64acbe60e68ff799600930d8b52b8a18
Harpreet Aulakh <hkaulakh14@gmail.com>
2020-05-20 22:50:24 -0500

    updated readme

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-// $(document).ready(function () {
+$(document).ready(function () {
+  //create a btn click event that fires when the searchMovie btn is clicked
+  //once btn click works the btn click should fire the ajax call
+  $("#searchMovieBtn").on("click", function (event) {
+    console.log("I've been clicked");
+    //search movie should equal to the value entered by the user from the input field with an id of userMovieInput
+    var searchMovie = $("#userMovieInput").val().trim();
+    console.log(searchMovie);
 
-//we need to make a btn click event that fires when the searchMovie btn is clicked
-//once btn click works the btn click should fire the ajax call
+    //here we build our query url by concating var searchMovie into our hard coded url
+    var queryUrl =
+      "https://www.googleapis.com/youtube/v3/search?type=video&q=" +
+      searchMovie +
+      " official trailer&key=AIzaSyB_pQRVeuodW7AAbb4hN2COspiJikOuXnQ";
+    console.log(queryUrl);
+    // Creating an AJAX call for the specific movie's trailer when serached
 
-$("#searchMovieBtn").on("click", function (event) {
-  console.log("I've been clicked");
-  //search movie should equal to the value entered by the user from the input field with an id of userMovieInput
-  var searchMovie = $("#userMovieInput").val().trim();
-  console.log(searchMovie);
-  //here we build our query url by concating var searchMovie into our hard coded url
-  var queryUrl =
-    "https://www.googleapis.com/youtube/v3/search?type=video&q=" +
-    searchMovie +
-    " official trailer&key=AIzaSyB_pQRVeuodW7AAbb4hN2COspiJikOuXnQ";
-  console.log(queryUrl);
-  // Creating an AJAX call for the specific movie's trailer when serached
+    $.ajax({
+      url: queryUrl,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response.items[0].id.videoId);
+      var youtubeVideoId = response.items[0].id.videoId;
+      var trailerUrl = "https://www.youtube.com/watch?v=" + youtubeVideoId;
 
-  $.ajax({
-    url: queryUrl,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response.items[0].id.videoId);
-    var youtubeVideoId = response.items[0].id.videoId;
-    var trailerUrl = "https://www.youtube.com/watch?v=" + youtubeVideoId;
-
-    console.log(trailerUrl);
+      console.log(trailerUrl);
+      $("#trailerDisplay").text(response.youtubeVideoId);
+    });
   });
 });
