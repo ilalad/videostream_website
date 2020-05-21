075201495bb81353253c561d3a2b19810488f59e
Derrick Chargois <36555200+dchargois23@users.noreply.github.com>
2020-05-20 19:58:21 -0500

    update1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=us",
+    "url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=Spiderman&country=us",
     "method": "GET",
     "headers": {
         "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
@@ -12,9 +12,6 @@ var settings = {
 $.ajax(settings).done(function (response) {
     console.log(response);
 });
-
-
-
 
 
 
